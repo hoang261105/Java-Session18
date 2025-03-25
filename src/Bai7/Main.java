2be7b42f4732342226cece381f1d91491cf5889c
hoang261105 <147359595+hoang261105@users.noreply.github.com>
2025-03-25 12:05:16 +0700

    Update Bai7

--- src/Bai7/Main.java
+++ src/Bai7/Main.java
@@ -1,8 +1,6 @@
 package Bai7;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,8 +22,17 @@ public class Main {
             counter.put(numbers[i], counter.getOrDefault(numbers[i], 0) + 1);
         }
 
-        for(Integer key : counter.keySet()){
-            System.out.println(key + " : " + counter.get(key));
+        List<Map.Entry<Integer, Integer>> sortNumbers = new ArrayList<>(counter.entrySet());
+
+        Collections.sort(sortNumbers, new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o1.getValue() - o2.getValue();
+            }
+        });
+
+        for(Map.Entry<Integer, Integer> entry : sortNumbers){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 }
